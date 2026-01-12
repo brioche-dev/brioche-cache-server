77b442bb03fd3ae9eb58a1f93e5dddaae8108896
Kyle Lacy <kylelacy@kyle.space>
2026-01-11 20:48:19 -0800

    Add some basic HTTP metrics

--- src/main.rs
+++ src/main.rs
@@ -48,37 +48,40 @@ async fn main() -> anyhow::Result<()> {
         store: Arc::new(store),
     };
 
-    let app = app::router(state).layer(
-        tower::ServiceBuilder::new().layer(
-            tower_http::trace::TraceLayer::new_for_http()
-                .make_span_with(|req: &axum::http::Request<_>| {
-                    let path =
-                        if let Some(path) = req.extensions().get::<axum::extract::MatchedPath>() {
+    let app = app::router(state)
+        .layer(axum::middleware::from_fn(request_metrics_middleware))
+        .layer(
+            tower::ServiceBuilder::new().layer(
+                tower_http::trace::TraceLayer::new_for_http()
+                    .make_span_with(|req: &axum::http::Request<_>| {
+                        let path = if let Some(path) =
+                            req.extensions().get::<axum::extract::MatchedPath>()
+                        {
                             path.as_str()
                         } else {
                             req.uri().path()
                         };
-                    let request_id = uuid::Uuid::new_v4();
-                    tracing::info_span!("request", path, %request_id)
-                })
-                .on_request(|_req: &axum::http::Request<_>, _span: &tracing::Span| {
-                    tracing::debug!("started request");
-                })
-                .on_response(
-                    |res: &axum::http::Response<_>,
-                     latency: std::time::Duration,
-                     _span: &tracing::Span| {
-                        tracing::debug!(
-                            latency_secs = latency.as_secs_f32(),
-                            response_code = res.status().as_u16(),
-                            "finished request",
-                        );
-                    },
-                ),
-        ),
-    );
+                        let request_id = uuid::Uuid::new_v4();
+                        tracing::info_span!("request", path, %request_id)
+                    })
+                    .on_request(|_req: &axum::http::Request<_>, _span: &tracing::Span| {
+                        tracing::debug!("started request");
+                    })
+                    .on_response(
+                        |res: &axum::http::Response<_>,
+                         latency: std::time::Duration,
+                         _span: &tracing::Span| {
+                            tracing::debug!(
+                                latency_secs = latency.as_secs_f32(),
+                                response_code = res.status().as_u16(),
+                                "finished request",
+                            );
+                        },
+                    ),
+            ),
+        );
 
-    let prometheus = metrics_exporter_prometheus::PrometheusBuilder::new().install_recorder()?;
+    let prometheus = install_prometheus_recorder()?;
     let metrics_app = axum::Router::new().route(
         "/metrics",
         axum::routing::get({
@@ -119,4 +122,46 @@ async fn main() -> anyhow::Result<()> {
     };
 
     Ok(())
+}
+
+fn install_prometheus_recorder() -> anyhow::Result<metrics_exporter_prometheus::PrometheusHandle> {
+    const EXPONENTIAL_SECONDS: &[f64] = &[
+        0.005, 0.01, 0.025, 0.05, 0.1, 0.25, 0.5, 1.0, 2.5, 5.0, 10.0,
+    ];
+
+    let prometheus = metrics_exporter_prometheus::PrometheusBuilder::new()
+        .set_buckets_for_metric(
+            metrics_exporter_prometheus::Matcher::Full(
+                "http_requests_duration_seconds".to_string(),
+            ),
+            EXPONENTIAL_SECONDS,
+        )?
+        .install_recorder()?;
+
+    Ok(prometheus)
+}
+
+async fn request_metrics_middleware(
+    req: axum::extract::Request,
+    next: axum::middleware::Next,
+) -> impl axum::response::IntoResponse {
+    let start = std::time::Instant::now();
+
+    let method = req.method().to_string();
+    let path = req
+        .extensions()
+        .get::<axum::extract::MatchedPath>()
+        .map_or_else(|| req.uri().path(), |path| path.as_str())
+        .to_owned();
+    let response = next.run(req).await;
+
+    let status = response.status().as_u16().to_string();
+    let labels = [("method", method), ("path", path), ("status", status)];
+
+    let duration = start.elapsed();
+
+    metrics::counter!("http_requests_total", &labels).increment(1);
+    metrics::histogram!("http_requests_duration_seconds", &labels).record(duration.as_secs_f64());
+
+    response
 }
